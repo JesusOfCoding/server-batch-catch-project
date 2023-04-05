b67f37a476dc84bf9233b00f376a495661e2ac7b
kimalss22 <kimalss22@gmail.com>
2023-04-05 18:30:17 +0900

    Initial commit

---
+++ src/test/java/com/side/serverbatchcatchproject/ServerBatchCatchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side.serverbatchcatchproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerBatchCatchProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
